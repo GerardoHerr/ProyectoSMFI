2c36626d5177ae1df3f0e4a1f53d765e48b90d74
Gerardo Herrera <gerardo.herrera@unl.edu.ec>
2026-05-05 20:20:02 -0500

    any workflow3

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -12,11 +12,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from flask import config
+import os
+from dotenv import load_dotenv
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
